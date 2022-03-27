6eb7243474d8190da2153ee3ebc554f5eeefda1f
ssgoncalves <samuel_gs@live.com>
2022-03-27 16:17:32 -0300

    feat: add render to BaseException

--- InfluenceIT/Authentication/Exceptions/InvalidCredentialsException.php
+++ InfluenceIT/Authentication/Exceptions/InvalidCredentialsException.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace InfluenceIT\Authentication\Exceptions;
 
 use InfluenceIT\Core\Exceptions\BaseException;
+use Symfony\Component\HttpFoundation\Response;
 
 class InvalidCredentialsException extends BaseException
 {
+    protected $message = 'authentication.credentials.invalid';
 
+    protected $code = Response::HTTP_FORBIDDEN;
 }
--- InfluenceIT/Core/Exceptions/BaseException.php
+++ InfluenceIT/Core/Exceptions/BaseException.php
@@ -5,8 +5,24 @@ declare(strict_types=1);
 namespace InfluenceIT\Core\Exceptions;
 
 use Exception;
+use Illuminate\Http\JsonResponse;
 
 class BaseException extends Exception
 {
+
+    public function render(): JsonResponse
+    {
+        $httpStatusCode = $this->code > 99 && $this->code < 600
+            ? $this->code
+            : 500;
+
+        $responseData = [
+            'message' => trans($this->message),
+            'code' => $this->message,
+            'errors' => $this->errors ?? [],
+        ];
+
+        return response()->json($responseData, $httpStatusCode);
+    }
 
 }
