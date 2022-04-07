b4e5e48c62056f14e5ae4f9127642c1b17188a0c
ssgoncalves <samuel_gs@live.com>
2022-04-07 00:51:13 -0300

    test(core): add base base exception Tests

--- tests/Unit/InfluenceIT/Core/Exceptions/BaseExceptionTest.php
+++ tests/Unit/InfluenceIT/Core/Exceptions/BaseExceptionTest.php
@@ -10,22 +10,41 @@ use Tests\TestCase;
 class BaseExceptionTest extends TestCase
 {
 
-    public function testShouldRenderWithValidCode(): void
+    /**
+     * @dataProvider getRenderScenarios
+     */
+    public function testShouldRenderWithValidCode(string $message, int $code, array $expected): void
     {
         // Set
-        $message = $this->faker->sentence;
-        $code = $this->faker->numberBetween(100,599);
-        $expected = array_merge(compact('message', 'code'), ['errors' => []]);
-
         $exception = new BaseException(message: $message, code: $code);
 
         // Actions
         $result = $exception->render();
 
         // Assertions
-        self::assertSame(expected: $code, actual: $result->status());
+        self::assertSame(expected: $expected['code'], actual: $result->status());
         self::assertSame(expected: $expected, actual: json_decode($result->content(), true));
 
+    }
+
+    public function getRenderScenarios(): array
+    {
+
+        $code = $this->faker->numberBetween(100, 599);
+        $message = $this->faker->sentence;
+
+        return [
+            'valid code' => [
+                'message' => $message,
+                'code' => $code,
+                'expected' => array_merge(compact('message', 'code'), ['errors' => []])
+            ],
+            'invalid code' => [
+                'message' => $message,
+                'code' => $this->faker->numberBetween(1000),
+                'expected' => array_merge(compact('message'), ['code' => 500, 'errors' => []])
+            ],
+        ];
     }
 
 }
